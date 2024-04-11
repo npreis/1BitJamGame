91db920c9ea200ac32f37636e409ac2c864735f1
npreis <nicholaspreis@gmail.com>
2024-04-11 17:15:52 -0400

    Made object detection system and corresponding UI

---
+++ 1BitJamGame/Assets/Scripts/InteractWithObjectScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractWithObjectScript : MonoBehaviour
+{
+    [SerializeField]
+    Camera cam;
+
+    [SerializeField]
+    GameObject interactUI;
+    bool canInteract;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canInteract = false;
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        RaycastHit hit;
+        float rayDistance = 1.5f;
+        Ray ray = new Ray(origin: cam.transform.position, direction: cam.transform.forward);
+        Debug.DrawRay(ray.origin, ray.direction * rayDistance);
+        if (Physics.Raycast(ray, out hit, rayDistance))
+        {
+            if (hit.collider.tag == "Interact")
+            {
+                canInteract = true;
+                interactUI.SetActive(true);
+            }
+            if (hit.collider.tag == "Untagged")
+            {
+                canInteract = false;
+                interactUI.SetActive(false);
+            }
+        }
+        else
+        {
+            canInteract = false;
+            interactUI.SetActive(false);
+        }
+    }
+}
