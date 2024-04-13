ece64c9a9292ec02dc75cd504272067a8dea9c4a
npreis <nicholaspreis@gmail.com>
2024-04-12 22:53:16 -0400

    Added main menu and it works.

---
+++ 1BitJamGame/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
